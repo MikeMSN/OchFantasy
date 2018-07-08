e5b1df74593610f4e72d5a525dd15a96adc95ba2
MicahMSN <jpackit@gmail.com>
2018-07-08 23:04:39 +0300

    guava playground actions + migrate to maven assembly plugin

--- src/main/java/mi/mo/Main.java
+++ src/main/java/mi/mo/Main.java
@@ -1,10 +1,43 @@
 package mi.mo;
 
+import com.google.common.base.Functions;
+import com.google.common.collect.*;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Maven");
+        playgroundActionsWithGuavaCollections();
+
+    }
+
+    private static void playgroundActionsWithGuavaCollections() {
+        BiMap<String, Integer> words = HashBiMap.create();
+        words.put("First", 1);
+        words.put("Second", 2);
+        words.put("Third", 3);
+        System.out.println(words.toString());
+        System.out.println(words.inverse().toString());
+
+        Multimap<String, String> multimap = ArrayListMultimap.create();
+        multimap.put("fruit", "apple");
+        multimap.put("fruit", "banana");
+        multimap.put("pet", "cat");
+        multimap.put("pet", "dog");
+        System.out.println(multimap.toString());
+        System.out.println(multimap.get("pet"));
+        System.out.println(multimap.values().toString());
+
+        Table<String,String,Integer> distance = HashBasedTable.create();
+        distance.put("London", "Paris", 340);
+        distance.put("New York", "Los Angeles", 3940);
+        distance.put("London", "New York", 5576);
+        System.out.println(distance.toString());
+        System.out.println(distance.cellSet());
     }
 
 }
